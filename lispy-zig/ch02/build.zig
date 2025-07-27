637d8f3897949ed2ed95a134c6209201eff8999b
netpyoung <netpyoung@gmail.com>
2025-07-28 07:33:56 +0900

    add: lispy-zig/ch02/

---
+++ lispy-zig/ch02/build.zig
@@ -0,0 +1,21 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "ch02",
+        .root_source_file = b.path("src/ch02.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // zig-out/bin/ 아래에 실행파일을 복사해 줌.
+    b.installArtifact(exe);
+
+    // zig build <원하는 스탭이름>을 하면 바이너리를 실행함.
+    const run_cmd = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_cmd.step);
+}
