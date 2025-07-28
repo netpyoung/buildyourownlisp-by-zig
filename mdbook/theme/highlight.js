558b74a74578c18329bc49724232610de57c4d01
netpyoung <netpyoung@gmail.com>
2025-07-29 06:53:45 +0900

    add: mdbook/theme/highlight.js

---
+++ mdbook/theme/highlight.js
@@ -0,0 +1,21 @@
+// ref: https://github.com/rust-lang/mdBook/issues/2467#issuecomment-2857375717
+
+let shikiModule
+
+window.hljs = {
+    configure() {
+        shikiModule = import('https://esm.sh/shiki@3.8.1')
+    },
+    /** @param {HTMLElement} block */
+    async highlightBlock(block) {
+        const lang = [...block.classList.values()]
+            .map(name => name.match(/^language-(.+)$/)?.[1])
+            .filter(Boolean)[0]
+        if (!lang) {
+            return
+        }
+        const shiki = await shikiModule
+        block.parentElement.innerHTML =
+            await shiki.codeToHtml(block.innerText, { lang, theme: 'one-light' })
+    }
+}
